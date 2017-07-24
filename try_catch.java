c667f82d512d64b99d0681c16b8c7b4aa6eee473
sachinsn231 <30321924+sachinsn231@users.noreply.github.com>
2017-07-24 23:45:21 +0530

    Factorial of 8
    Just debugging the code

--- try_catch.java
+++ try_catch.java
@@ -3,14 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Calculate(5));
+        System.out.println(Calculate(8));
     }
 
     public static int Calculate(int number) {
         if (number == 1) {
             return 1;
         }
-        return Calculate(number - 1) * number;
-
+        return Calculate(number - 1) * number; // Just debugging the code
     }
 }
